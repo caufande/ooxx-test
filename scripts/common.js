cb2981ecc02b7aba3341f0bcbdc73a8274ea8422
caufande <3823642480@qq.com>
2024-12-06 22:43:46 +0800

    doc: 注释

--- scripts/common.js
+++ scripts/common.js
@@ -3,6 +3,12 @@ onload = () => {
 	changePage('start');
 }
 
+/**
+ * ### 切换当前页面为 `pageName`
+ *
+ * 需要通过 {@link changePage.init} 初始化
+ * @param {string} pageName 要切换到的页面名称
+ */
 function changePage(pageName) {
 	for (const page of changePage.list) {
 		page.style.display = page.dataset.pageName === pageName ? 'block' : '';
